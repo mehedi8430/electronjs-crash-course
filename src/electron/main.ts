9441d11967d91d6b066ea593074f3e906e31ed0d
MEHEDI HASAN <mehedialhasan1999@gmail.com>
2025-01-15 20:44:38 +0600

    first commit

---
+++ src/electron/main.ts
@@ -0,0 +1,71 @@
+import { app, BrowserWindow } from "electron";
+import { createMenu } from "./menu.js";
+import { getPreloadPath, getUIPath } from "./pathResolver.js";
+import { getStaticData, pollResources } from "./resourceManager.js";
+import { createTray } from "./tray.js";
+import { ipcMainHandle, ipcMainOn, isDev } from "./util.js";
+
+app.on("ready", () => {
+  const mainWindow = new BrowserWindow({
+    webPreferences: {
+      preload: getPreloadPath(),
+    },
+    // disables default system frame (dont do this if you want a proper working menu bar)
+    frame: false,
+  });
+
+  if (isDev()) {
+    mainWindow.loadURL("http://localhost:5123");
+  } else {
+    mainWindow.loadFile(getUIPath());
+  }
+
+  pollResources(mainWindow);
+
+  ipcMainHandle("getStaticData", () => {
+    return getStaticData();
+  });
+
+  ipcMainOn("sendFrameAction", (payload) => {
+    switch (payload) {
+      case "CLOSE":
+        mainWindow.close();
+        break;
+      case "MAXIMIZE":
+        mainWindow.maximize();
+        break;
+      case "MINIMIZE":
+        mainWindow.minimize();
+        break;
+    }
+  });
+
+  createTray(mainWindow);
+  handleCloseEvents(mainWindow);
+  createMenu(mainWindow);
+});
+
+function handleCloseEvents(mainWindow: BrowserWindow) {
+  let willClose = false;
+
+  mainWindow.on("close", (e) => {
+    if (willClose) {
+      return;
+    }
+
+    e.preventDefault();
+    mainWindow.hide();
+
+    if (app.dock) {
+      app.dock.hide();
+    }
+  });
+
+  app.on("before-quit", () => {
+    willClose = true;
+  });
+
+  mainWindow.on("show", () => {
+    willClose = false;
+  });
+}
